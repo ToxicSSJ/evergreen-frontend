f9aab96ff49ef3439d4af358cfd4e05fde2cd51f
Juan Felipe Ramirez Ramirez <jframirezr@itcol.com>
2023-03-25 07:10:39 -0500

    avance

--- src/services/evergreen.sertive.ts
+++ src/services/evergreen.sertive.ts
@@ -11,7 +11,9 @@ export class EvergreenService {
 
   predio: string = environment.apiUrl + '/api/plots';
 
-  ganancia: string = environment.apiUrl + '/api/profit'
+  ganancia: string = environment.apiUrl + '/api/profit';
+
+  prediccion: string = environment.apiUrl + '/api/prediction';
 
   constructor(private _httpClient: HttpClient) {
   }
@@ -34,5 +36,17 @@ export class EvergreenService {
 
   actualizarGanancia(body:any): Observable<any>{
     return this._httpClient.put(this.ganancia, body);
+  }
+
+  getPrediction(): Observable<any>{
+    return this._httpClient.get(this.prediccion);
+  }
+
+  setPrediction(body:any): Observable<any>{
+    return this._httpClient.post(this.prediccion, body);
+  }
+
+  actualizarPrediction(body:any): Observable<any> {
+    return this._httpClient.put(this.prediccion, body);
   }
 }
